78f52be4f1741362ce5c8a81bf3da289d8c1b047
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 18:00:15 +0500

    I added routes for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,21 @@
 <?php
 
-use App\Http\Controllers\CategoryProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoryProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
 Route::apiResources([
     '/categories' => CategoryController::class,
     '/products' => ProductController::class,
     '/categories/{id}/products' => CategoryProductController::class
 ]);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('verify', [AuthController::class, 'verifyEmail']);
