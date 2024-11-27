cae6daeb831152444ab10529c26fc908005afbd7
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 16:39:02 +0500

    I change relation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
 
     public function index()
     {
-        $products = Product::with('category')->paginate(25);
+        $products = Product::with('stocks')->paginate(25);
         return response()->json([
             'products' => ProductResource::collection($products),
             'links' => [
