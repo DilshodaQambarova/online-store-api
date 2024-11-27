8f3ce6d33197da7df0f868672d577ad70501370e
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 16:38:28 +0500

    I added pagination

--- app/Http/Controllers/CategoryProductController.php
+++ app/Http/Controllers/CategoryProductController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryProductController extends Controller
 {
-    public function index($id){
-        return Category::with('products')->cursorPaginate(10)->findOrFail($id);
-    }
+    public function index($id)
+{
+    $category = Category::with('products.stocks')
+        ->where('id', $id)
+        ->firstOrFail();
+
+    $products = $category->products()->paginate(10);
+
+    return response()->json([
+        'category' => new CategoryResource($category->load('productsd')),
+        'links' => [
+            'first' => $products->url(1),
+            'last' => $products->url($products->lastPage()),
+            'prev' => $products->previousPageUrl(),
+            'next' => $products->nextPageUrl(),
+        ],
+        'meta' => [
+            'current_page' => $products->currentPage(),
+            'from' => $products->firstItem(),
+            'last_page' => $products->lastPage(),
+            'path' => $products->path(),
+            'per_page' => $products->perPage(),
+            'to' => $products->lastItem(),
+            'total' => $products->total(),
+        ],
+    ]);
+}
+
 }
