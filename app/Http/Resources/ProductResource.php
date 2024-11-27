9c57bbfa4dc9203f91340168cb5b9dac915e900e
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 11:52:34 +0500

    I created resource for get only neccesary column's from products table

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $name = json_decode($this->name);
+        $description = json_decode($this->description);
+        return [
+            'id' => $this->id,
+            'name_uz' => $name->uz,
+            'name_ru' => $name->ru,
+            'price' => $this->price,
+            'description_uz' => $description->uz,
+            'description_ru' => $description->ru,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
