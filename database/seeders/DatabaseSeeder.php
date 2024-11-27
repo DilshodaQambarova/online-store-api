28df677e31a677bca5277dcf69e489b7700d6670
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 12:12:48 +0500

    I have registered all seeders from database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Database\Seeders\ValueSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            CategorySeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
+            AttributeSeeder::class,
+            ValueSeeder::class,
+            ProductSeeder::class
+        ]);
+    }
+}
