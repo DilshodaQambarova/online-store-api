9201b16af468ddf3dd82503de3f7248dab37603d
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 11:45:36 +0500

    I created controller for product but I didn't write CRUD operations I wrote only index and show methods

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::with('category')->paginate(25);
+        return response()->json([
+            'products' => ProductResource::collection($products),
+            'links' => [
+                'first' => $products->url(1),
+                'last' => $products->url($products->lastPage()),
+                'prev' => $products->previousPageUrl(),
+                'next' => $products->nextPageUrl(),
+            ],
+            'meta' => [
+                'current_page' => $products->currentPage(),
+                'from' => $products->firstItem(),
+                'last_page' => $products->lastPage(),
+                'path' => $products->path(),
+                'per_page' => $products->perPage(),
+                'to' => $products->lastItem(),
+                'total' => $products->total(),
+            ],
+        ]);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return Product::with('stocks', 'category')->findOrFail($id);
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
