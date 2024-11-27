d7379eb8b3c98fe099ed1b53c1571db5bd82bc2b
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 12:11:02 +0500

    I created attributes through AttributeSeeder

---
+++ database/seeders/AttributeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Attribute;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AttributeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $attributes = [
+            ['name' => 'color'],
+            ['name' => 'material'],
+            ['name' => 'size'],
+        ];
+        Attribute::insert($attributes);
+    }
+}
