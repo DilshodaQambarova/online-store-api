ea6f2d2c729e56788dd1db8c2e08bbe97513110a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 17:56:33 +0500

    I created mail for send verification sms to user's email

---
+++ app/Mail/SendSmsToMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendSmsToMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Email verification',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        $link = 'http://localhost:8000/verify?=' . $this->user->verification_token;
+        return new Content(
+            view: 'email.send',
+            with:[
+                'name' => $this->user->name,
+                'link' => $link
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
