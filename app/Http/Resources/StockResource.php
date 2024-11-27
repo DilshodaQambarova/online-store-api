049690d05a9254932dde18e4ebbaa49364b800c9
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 16:40:40 +0500

    I added resource for Stockresource

--- app/Http/Resources/StockResource.php
+++ app/Http/Resources/StockResource.php
@@ -14,6 +14,22 @@ class StockResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $attributes = json_decode($this->attributes, true);
+        return [
+            'id' => $this->id,
+            'attributes' => [
+
+                [
+                    'attribute_id' => $attributes[0]['attribute_id'],
+                    'value_id' => $attributes[0]['value_id'],
+                ],
+                [
+                    'attribute_id' => $attributes[1]['attribute_id'],
+                    'value_id' => $attributes[1]['value_id'],
+                ],
+            ],
+
+            'quantity' => $this->quantity
+        ];
     }
 }
