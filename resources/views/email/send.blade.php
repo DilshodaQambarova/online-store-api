3f581288577cd3c9741a35ac82a4468092f08049
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-27 17:57:55 +0500

    I wrote message for send email

--- resources/views/email/send.blade.php
+++ resources/views/email/send.blade.php
@@ -1,3 +1,4 @@
 <div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+    <h1>Welcome, {{$name}}!</h1>
+    <a href="{{$link}}">Email Verification</a>
 </div>
