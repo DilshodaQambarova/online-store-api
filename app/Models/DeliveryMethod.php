d01c1bf860191fa8e8a980e685f5b6d3314dda90
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-28 22:12:20 +0500

    I created model for DeliveryMethod class and I wrote fillable, translatable and relation method hasMany with order

---
+++ app/Models/DeliveryMethod.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class DeliveryMethod extends Model
+{
+    /** @use HasFactory<\Database\Factories\DeliveryMethodFactory> */
+    use HasFactory, HasTranslations;
+    protected $fillable = [
+        'name',
+        'estimated_time',
+        'price'
+    ];
+    public array $translatable = [
+        'name',
+        'estimated_time'
+    ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
