b1ff6e9f1d261f3142621485206f6fb204a32900
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-28 22:11:27 +0500

    I created model for  order class and I wrote many relation methods with user, with delivery method and with payment type

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,22 @@ class Order extends Model
 {
     /** @use HasFactory<\Database\Factories\OrderFactory> */
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'payment_type_id',
+        'delivery_method_id',
+        'sum',
+        'comment',
+        'products',
+        'address',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function deliveryMethod(){
+        return $this->belongsTo(DeliveryMethod::class);
+    }
+    public function paymentType(){
+        return $this->belongsTo(PaymentType::class);
+    }
 }
