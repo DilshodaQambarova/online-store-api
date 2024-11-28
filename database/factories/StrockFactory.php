c11bf1bac6e6b00fb7509768b2a5097766f9fa42
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-28 14:24:35 +0500

    I fixed mistake

--- database/factories/StrockFactory.php
+++ database/factories/StrockFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Strock>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stock>
  */
 class StrockFactory extends Factory
 {
