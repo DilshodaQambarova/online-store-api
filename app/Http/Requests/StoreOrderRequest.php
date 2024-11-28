7a8747004b79079029e72d8288697e0db02cc2d7
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-28 22:14:41 +0500

    I created request class for validate order data and I wrote validations

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -11,7 +11,7 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StoreOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'comment' => 'nullable|string|max:225',
+            'delivery_method_id' => 'required',
+            'payment_type_id' => 'required',
+            'products' => 'required',
+            'sum' => 'required|numeric'
         ];
     }
 }
