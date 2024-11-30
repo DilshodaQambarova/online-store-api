8ea69e62cc367fdf650acdf7c10eb0e6b6e22f1e
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-30 14:38:22 +0500

    I created property casts for casting payment type  from json to array

--- app/Models/PaymentType.php
+++ app/Models/PaymentType.php
@@ -2,18 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PaymentType extends Model
 {
     /** @use HasFactory<\Database\Factories\PaymentTypeFactory> */
-    use HasFactory, HasTranslations;
+    use HasFactory, HasTranslations, SoftDeletes;
     protected $fillable = [
         'name'
     ];
     public array $translatable = ['name'];
+    protected $casts = [
+        'paymentType' => 'array'
+    ];
     public function orders(){
         return $this->hasMany(Order::class);
     }
