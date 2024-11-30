04014b7ae10c54d774b587a7775d1830233281ee
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-30 22:02:21 +0500

    I changed index method

--- app/Http/Controllers/CategoryProductController.php
+++ app/Http/Controllers/CategoryProductController.php
@@ -4,36 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryProductController extends Controller
 {
     public function index($id)
-{
-    $category = Category::with('products.stocks')
-        ->where('id', $id)
-        ->firstOrFail();
+    {
+        $category = Category::with('products.stocks')
+            ->where('id', $id)
+            ->first();
 
-    $products = $category->products()->paginate(10);
-
-    return response()->json([
-        'category' => new CategoryResource($category->load('products')),
-        'links' => [
-            'first' => $products->url(1),
-            'last' => $products->url($products->lastPage()),
-            'prev' => $products->previousPageUrl(),
-            'next' => $products->nextPageUrl(),
-        ],
-        'meta' => [
-            'current_page' => $products->currentPage(),
-            'from' => $products->firstItem(),
-            'last_page' => $products->lastPage(),
-            'path' => $products->path(),
-            'per_page' => $products->perPage(),
-            'to' => $products->lastItem(),
-            'total' => $products->total(),
-        ],
-    ]);
-}
+        $products = $category->products()->paginate(10);
+        if (!$category || !$products) {
+            return $this->error('Not found', 404);
+        }
+        return $this->responsePagination($products, new CategoryResource($category->load('products')));
+    }
 
 }
