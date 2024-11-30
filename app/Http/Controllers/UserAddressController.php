745f3656f693eea8e16471cfaa0dfc15e7e8d92d
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-30 22:03:12 +0500

    I changed store method

--- app/Http/Controllers/UserAddressController.php
+++ app/Http/Controllers/UserAddressController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserAddress;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\AddressResource;
 use App\Http\Requests\StoreUserAddressRequest;
 use App\Http\Requests\UpdateUserAddressRequest;
 
@@ -19,10 +20,8 @@ class UserAddressController extends Controller
 
     public function store(StoreUserAddressRequest $request)
     {
-        Auth()->user()->addresses()->create($request->toArray());
-        return response()->json([
-            'message' => 'Address created'
-        ], 201);
+        $address = Auth()->user()->addresses()->create($request->toArray());
+        return $this->success(new AddressResource($address), 'Address created', 201);
     }
 
     public function show(UserAddress $userAddress)
