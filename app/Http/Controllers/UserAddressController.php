d414401e47964359b605a6df22be24186a983809
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-30 22:07:27 +0500

    I changed index method in UserAddressController

--- app/Http/Controllers/UserAddressController.php
+++ app/Http/Controllers/UserAddressController.php
@@ -13,7 +13,8 @@ class UserAddressController extends Controller
 
     public function index()
     {
-        return Auth::user()->addresses;
+        $addresses = Auth::user()->addresses;
+        return $this->success(AddressResource::collection($addresses));
     }
 
 
