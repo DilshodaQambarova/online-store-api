e71254266140e26f1a0227f39d5a871b0e3d8001
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-01 17:28:49 +0500

    I wrote validation for save edited address data

--- app/Http/Requests/UpdateUserAddressRequest.php
+++ app/Http/Requests/UpdateUserAddressRequest.php
@@ -11,7 +11,7 @@ class UpdateUserAddressRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class UpdateUserAddressRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'latitude' => 'nullable',
+            'longitude' => 'nullable',
+            'region' => 'nullable',
+            'district' => 'nullable',
+            'street' => 'nullable',
+            'home' => 'nullable'
         ];
     }
 }
