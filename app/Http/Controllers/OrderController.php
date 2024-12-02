a9e1302fd9974764dbca9045b21c20dac87a5eb9
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-02 17:08:13 +0500

    I wrote foreach and if statement from create order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\UpdateOrderRequest;
+use App\Http\Resources\OrderResource;
+use App\Http\Resources\ProductResource;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\UserAddress;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Resources\OrderResource;
-use App\Http\Requests\StoreOrderRequest;
-use App\Http\Requests\UpdateOrderRequest;
 
 class OrderController extends Controller
 {
@@ -21,12 +22,24 @@ class OrderController extends Controller
         return OrderResource::collection($orders->load('paymentType', 'deliveryMethod', 'user'));
     }
 
-
     public function store(StoreOrderRequest $request)
     {
         $sum = 0;
-        $products = Product::query()->limit(2)->get();
+        $products = [];
         $address = UserAddress::findOrFail($request->address_id);
+        foreach ($request->products as $product) {
+            $prod = Product::with('stocks')->findOrFail($product['product_id']);
+            if (is_array($product) && isset($product['product_id'], $product['stock_id'], $product['quantity'])) {
+
+                if (
+                    $prod->stocks()->find($product['stock_id']) &&
+                    $prod->stocks()->find($product['stock_id'])->quantity >= $product['quantity']
+                ) {
+                   $p = $prod->withStock(6);
+                   return new ProductResource($p);
+                }
+            }
+        }
         $order = new Order();
         $order->user_id = Auth::id();
         $order->payment_type_id = $request->payment_type_id;
@@ -41,14 +54,10 @@ class OrderController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Order $order)
     {
         return new OrderResource($order->load('user', 'deliveryMethod', 'paymentType'));
     }
-
 
     public function update(UpdateOrderRequest $request, $id)
     {
@@ -56,7 +65,7 @@ class OrderController extends Controller
         $address = UserAddress::findOrFail($request->address_id);
 
         $order = Order::find($id);
-        if(!$order){
+        if (!$order) {
             return $this->error('Order not found', 404);
         }
         $order->payment_type_id = $request->payment_type_id;
