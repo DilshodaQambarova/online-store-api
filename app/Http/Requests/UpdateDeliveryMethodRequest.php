f54665d642cc3867c323f0dc4279884218c87d4a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-26 17:10:02 +0500

    Added validations for validate edited delivery method's data

--- app/Http/Requests/UpdateDeliveryMethodRequest.php
+++ app/Http/Requests/UpdateDeliveryMethodRequest.php
@@ -11,7 +11,7 @@ class UpdateDeliveryMethodRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateDeliveryMethodRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'nullable',
+            'estimated_time' => 'nullable|date',
+            'price' => 'nullable|decimal:1,500'
         ];
     }
 }
