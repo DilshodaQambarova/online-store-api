5ca585642ebe88ba79ce0865e6cff30542a0110c
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-30 19:42:53 +0500

    Added store method to StatusController

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -16,25 +16,15 @@ class StatusController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreStatusRequest $request)
     {
-        //
+        $status = new Status();
+        $status->name = $request->name;
+        $status->save();
+        return $this->success($status, 'Status creatde successfully', 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Status $status)
     {
         //
