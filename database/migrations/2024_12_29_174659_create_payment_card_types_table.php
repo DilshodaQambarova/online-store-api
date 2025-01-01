f88b8d875e245418a8198de9225c24ee86f91a4b
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-01 21:45:35 +0500

    Renamed table

--- database/migrations/2024_12_29_174659_create_payment_card_types_table.php
+++ database/migrations/2024_12_29_174659_create_payment_card_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payment_card_ttypes', function (Blueprint $table) {
+        Schema::create('payment_card_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('code');
