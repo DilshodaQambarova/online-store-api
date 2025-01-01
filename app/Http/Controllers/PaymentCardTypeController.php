90f10e3648e19c5285378cea0e4aad70e8fe14cf
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-01 21:47:27 +0500

    Added index method to PaymentCardTypeController

--- app/Http/Controllers/PaymentCardTypeController.php
+++ app/Http/Controllers/PaymentCardTypeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PaymentCardType;
 
 class PaymentCardTypeController extends Controller
 {
-    //
+    public function index(){
+        return $this->success(PaymentCardType::all());
+    }
 }
