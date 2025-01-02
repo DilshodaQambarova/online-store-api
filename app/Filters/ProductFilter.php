c2014707bc624383db8a0bdd0fbb7d0ae7535843
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-02 19:31:39 +0500

    Added full filter code

--- app/Filters/ProductFilter.php
+++ app/Filters/ProductFilter.php
@@ -2,6 +2,12 @@
 
 class ProductFilter {
     public function apply($filters = [], $query){
-
+        if($filters['name']){
+            $query->where('name', $filters['name']);
+        }
+        if($filters['price']){
+            $query->where('price', $filters['price']);
+        }
+        return $query;
     }
 }
