2e388fb173e6748153ac10d62894a114ea210250
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-02 19:48:16 +0500

    Added columns to attachments table

--- database/migrations/2025_01_02_144140_create_attachments_table.php
+++ database/migrations/2025_01_02_144140_create_attachments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
+            $table->string('path');
+            $table->unsignedBigInteger('attachmentable_id');
+            $table->string('attachmentable_type');
             $table->timestamps();
         });
     }
