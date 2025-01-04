44e1e61f091bd7f20facd636eb09fcd3d15a6516
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-04 16:29:12 +0500

    Removed method update from FavoriteController

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -8,18 +8,13 @@ use App\Http\Resources\ProductResource;
 
 class FavoriteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
         $favorites = Auth()->user()->favorites()->paginate(10);
         return $this->responsePagination($favorites, ProductResource::collection($favorites));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -31,9 +26,6 @@ class FavoriteController extends Controller
         return $this->success([], 'Product added to favorites', 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
        $favorite = Auth::user()->favorites()->find($id);
@@ -43,17 +35,6 @@ class FavoriteController extends Controller
        return $this->success(new ProductResource($favorite));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         if(!Auth::user()->hasFavorite($id)){
