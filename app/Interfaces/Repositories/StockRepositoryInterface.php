59c19a2bbb1a68b62782d82ed53e764a4eccf311
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-15 20:34:19 +0500

    Created interface for stock repository

---
+++ app/Interfaces/Repositories/StockRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Repositories;
+
+class StockRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
