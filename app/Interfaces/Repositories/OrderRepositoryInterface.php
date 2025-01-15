4f6f0be9456ccc3eab3ff700073bd5f3b924ec66
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-15 20:33:51 +0500

    Created interface for order repository

---
+++ app/Interfaces/Repositories/OrderRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Repositories;
+
+class OrderRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
