ad1c23d567cfc52296dc35fb98a588ac18a943a8
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-15 20:34:07 +0500

    Created interface for payment card repository

---
+++ app/Interfaces/Repositories/PaymentCardRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Repositories;
+
+class PaymentCardRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
