df252d163f914ebfe11d3fcc50a6c3b33a000b86
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-15 20:34:45 +0500

    Created interface for payment card service

---
+++ app/Interfaces/Services/PaymentCardServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Services;
+
+class PaymentCardServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
