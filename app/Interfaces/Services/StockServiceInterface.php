fca2cd15086006993a4ae4ea042cf598d0f50eca
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-15 20:34:56 +0500

    Created interface for stock service

---
+++ app/Interfaces/Services/StockServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Services;
+
+class StockServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
